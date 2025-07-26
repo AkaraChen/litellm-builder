1a47032eb00f14b10052717c6ea7c5906d14fe29
AkaraChen <akarachen@outlook.com>
2025-07-26 16:33:25 +0800

    chore: format

--- config-example/index.ts
+++ config-example/index.ts
@@ -3,16 +3,16 @@ import { OpenAICompat } from "@/providers/openai-compat.ts";
 import { from_env } from "@/utils/params.ts";
 
 export default build_config({
-    providers: [
-        new OpenAICompat({
-            name: "openai",
-            api_keys: [
-                from_env`OPENAI_API_KEY`
-            ],
-            api_base: "https://api.openai.com/v1",
-            models: [
-                "gpt-4o",
-            ]
-        })
-    ]
-})
+  providers: [
+    new OpenAICompat({
+      name: "openai",
+      api_keys: [
+        from_env`OPENAI_API_KEY`,
+      ],
+      api_base: "https://api.openai.com/v1",
+      models: [
+        "gpt-4o",
+      ],
+    }),
+  ],
+});
