6975ed3099c362989a726808cb21692096a81960
AkaraChen <akarachen@outlook.com>
2025-08-09 21:17:18 +0800

    feat: add new models

---
+++ src/models/doubao.ts
@@ -0,0 +1,7 @@
+export enum DoubaoModel {
+  "doubao-seed-1-6" = "doubao-seed-1-6",
+  "doubao-seed-1-6-thinking" = "doubao-seed-1-6-thinking",
+  "doubao-seed-1-6-flash" = "doubao-seed-1-6-flash",
+}
+
+export const doubao_models = Object.values(DoubaoModel);
